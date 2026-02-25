9af544918462cb0c44424c6080944f4ee52c1105
Yulia <yulia.tatarkina3@gmail.com>
2026-02-25 22:57:33 +0300

    Checkstyle

--- stats/stats-server/src/main/java/ru/practicum/controller/StatsController.java
+++ stats/stats-server/src/main/java/ru/practicum/controller/StatsController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.dto.response.EndPointHitDto;
 import ru.practicum.dto.request.EndPointHitDtoNew;
 import ru.practicum.dto.response.ViewStatsDto;
 import ru.practicum.service.StatsService;
