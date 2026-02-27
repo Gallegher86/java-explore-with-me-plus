b17e4599401cd8985d456b02e89ea82e0a628995
Anton Ryabushkin <riabushkinanton@yandex.ru>
2026-02-27 10:10:19 +0300

    fix: Изменить timestamp на LocalDateTime.

--- stats/stats-dto/src/main/java/ru/practicum/dto/request/EndPointHitDtoNew.java
+++ stats/stats-dto/src/main/java/ru/practicum/dto/request/EndPointHitDtoNew.java
@@ -1,11 +1,15 @@
 package ru.practicum.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 /**
  * DTO для создания нового хита конечной точки.
@@ -29,6 +33,7 @@ public class EndPointHitDtoNew {
     @Size(max = 45, message = "Длина IP-адреса не должна превышать 45 символов.")
     private String ip;
 
-    @NotBlank(message = "Время запроса должно быть указано.")
-    private String timestamp;
+    @NotNull(message = "Время запроса должно быть указано.")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
 }
