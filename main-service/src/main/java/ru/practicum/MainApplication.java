798b98638af213d088ef4916d7b0d173cca71c01
Anton Ryabushkin <riabushkinanton@yandex.ru>
2026-03-04 16:49:01 +0300

    fix: правки.

--- main-service/src/main/java/ru/practicum/MainApplication.java
+++ main-service/src/main/java/ru/practicum/MainApplication.java
@@ -2,15 +2,6 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import ru.practicum.dto.request.EndPointHitDtoNew;
-import ru.practicum.dto.request.ViewStatsParamDto;
-import ru.practicum.dto.response.ViewStatsDto;
-import ru.practicum.exceptions.StatsClientException;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @SpringBootApplication
 public class MainApplication {
