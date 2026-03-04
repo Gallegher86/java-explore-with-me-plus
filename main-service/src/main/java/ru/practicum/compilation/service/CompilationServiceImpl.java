2f9144afd63eb8840750ac5d7295d677758cc437
Anton Ryabushkin <riabushkinanton@yandex.ru>
2026-03-04 13:08:19 +0300

    fix: правки стиля.

--- main-service/src/main/java/ru/practicum/compilation/service/CompilationServiceImpl.java
+++ main-service/src/main/java/ru/practicum/compilation/service/CompilationServiceImpl.java
@@ -16,7 +16,6 @@ import ru.practicum.event.repository.EventRepository;
 import ru.practicum.exception.NotFoundException;
 import org.springframework.data.domain.Pageable;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
