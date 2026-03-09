1b61964724b094a1554e18320efa601d01527073
Anton Ryabushkin <riabushkinanton@yandex.ru>
2026-03-09 22:38:38 +0300

    fix: правки стиля.

--- main-service/src/main/java/ru/practicum/comment/service/CommentService.java
+++ main-service/src/main/java/ru/practicum/comment/service/CommentService.java
@@ -3,6 +3,7 @@ package ru.practicum.comment.service;
 import ru.practicum.comment.dto.*;
 
 import org.springframework.data.domain.Pageable;
+
 import java.util.List;
 
 public interface CommentService {
@@ -15,8 +16,8 @@ public interface CommentService {
     List<CommentShortDto> approveComments(CommentStatusUpdateRequest request);
 
     List<CommentFullDto> getComments(AdminCommentFilterParams params, Pageable pageable);
-    
+
     CommentFullDto getCommentById(Long commentId);
-    
+
     void deleteByAdmin(Long commentId);
 }
